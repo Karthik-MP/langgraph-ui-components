07c5d5e7af7ccb532cb7ae59a59694cf8bf00b73
Karthik <karthikmp521@gmail.com>
2026-01-04 23:51:17 -0500

    updating new changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build:{
-    lib:{
-      entry:"src/index.ts",
-      name:"ChatUI",
-      fileName:(format)=>`index.${format}.js`
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "ChatUI",
+      formats: ["es"],
+      fileName: (format) => `index.${format}.js`,
     },
-    rollupOptions:{
-      external:["React","React-dom"]
-    }
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+      ],
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@providers': path.resolve(__dirname, 'src/providers')
-    }
-  }
+      "@": path.resolve(__dirname, "src"),
+      "@providers": path.resolve(__dirname, "src/providers"),
+    },
+  },
 });
