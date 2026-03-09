d635fcb2bcfa9b7749008cdef281e16bfc92b8f1
Karthik <karthikmp521@gmail.com>
2026-03-09 14:22:13 -0400

    updating import strcuture with subscoped import

---
+++ src/entries/components.ts
@@ -0,0 +1,14 @@
+// ─── Components ───────────────────────────────────────────────────────────────
+export { default as Sidebar } from "../pages/Sidebar/sidebar";
+export { Chat } from "../pages/Chat/Chat";
+
+// ─── Types ────────────────────────────────────────────────────────────────────
+export type {
+  ChatProps,
+  ChatSidebarProps,
+  ChatUIProps,
+  CallThisOnSubmitResponse,
+  chatBodyProps,
+  headerProps,
+  textToSpeechVoice,
+} from "../types/ChatProps";
